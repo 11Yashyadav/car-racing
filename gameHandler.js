0a1fbc319090f8cdadd6e2d3a59cdabfa24fa7e6
Yash Yadav <yashyadav9325@gmail.com>
2025-05-13 11:34:49 +0530

    adjust difficulty by speed

--- gameHandler.js
+++ gameHandler.js
@@ -91,3 +91,62 @@ function onGameOver() {
     player.score +
     "<br> Press here to restart the game.";
 }
+
+function moveRoadLines() {
+  let roadLines = document.querySelectorAll(".roadLines");
+  roadLines.forEach((item) => {
+    if (item.y >= 700) {
+      item.y -= 750;
+    }
+    item.y += player.speed;
+    item.style.top = item.y + "px";
+  });
+}
+
+function moveEnemyCars(carElement) {
+  let enemyCars = document.querySelectorAll(".enemyCar");
+  enemyCars.forEach((item) => {
+    if (onCollision(carElement, item)) {
+      onGameOver();
+    }
+    if (item.y >= 750) {
+      item.y = -300;
+      item.style.left = Math.floor(Math.random() * 350) + "px";
+    }
+    item.y += player.speed;
+    item.style.top = item.y + "px";
+  });
+}
+
+function gamePlay() {
+  let carElement = document.querySelector(".car");
+  let road = gameArea.getBoundingClientRect();
+
+  if (player.start) {
+    moveRoadLines();
+    moveEnemyCars(carElement);
+
+    if (keys.ArrowUp && player.y > road.top + 70) player.y -= player.speed;
+    if (keys.ArrowDown && player.y < road.bottom - 85) player.y += player.speed;
+    if (keys.ArrowLeft && player.x > 0) player.x -= player.speed;
+    if (keys.ArrowRight && player.x < road.width - 70) player.x += player.speed;
+
+    carElement.style.top = player.y + "px";
+    carElement.style.left = player.x + "px";
+
+    window.requestAnimationFrame(gamePlay);
+
+    player.score++;
+    const ps = player.score - 1;
+    score.innerHTML = "Score: " + ps;
+  }
+}
+document.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  keys[e.key] = true;
+});
+
+document.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  keys[e.key] = false;
+});
